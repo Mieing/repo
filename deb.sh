3d3face491a8acb41c0acd2ac1a59f5ab475616f
Mieing <xiaohao_884@outlook.com>
2024-08-11 19:26:54 +0800

    增加插件

---
+++ deb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 临时文件用于合并内容
+temp_packages="./repo/temp_packages"
+
+# 确保临时目录存在
+mkdir -p "$temp_packages"
+
+# 扫描 rootfull 目录并生成 Packages 文件
+dpkg-scanpackages ./debs/rootfull /dev/null > "$temp_packages/Packages_rootfull"
+
+# 扫描 rootless 目录并生成 Packages 文件
+dpkg-scanpackages ./debs/rootless /dev/null > "$temp_packages/Packages_rootless"
+
+# 合并 Packages 文件
+cat "$temp_packages/Packages_rootfull" "$temp_packages/Packages_rootless" > "./repo/Packages"
+
+# 生成合并后的 Packages 文件并压缩
+gzip -9c /Packages > /Packages.gz
+bzip2 -9c /Packages > /Packages.bz2
+
+# 清理临时目录
+rm -rf "$temp_packages"
