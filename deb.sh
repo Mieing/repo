facb1b47a2ca951d21b4ce48231d941873c1cf25
Mieing <xiaohao_884@outlook.com>
2024-10-23 23:08:28 +0800

    lcon

--- deb.sh
+++ deb.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
 
 # 临时文件用于合并内容
-temp_packages="./repo/temp_packages"
+temp_packages="./temp_packages"
 
 # 确保临时目录存在
 mkdir -p "$temp_packages"
@@ -13,11 +13,11 @@ dpkg-scanpackages ./debs/rootfull /dev/null > "$temp_packages/Packages_rootfull"
 dpkg-scanpackages ./debs/rootless /dev/null > "$temp_packages/Packages_rootless"
 
 # 合并 Packages 文件
-cat "$temp_packages/Packages_rootfull" "$temp_packages/Packages_rootless" > "./repo/Packages"
+cat "$temp_packages/Packages_rootfull" "$temp_packages/Packages_rootless" > "./Packages"
 
 # 生成合并后的 Packages 文件并压缩
-gzip -9c "./repo/Packages" > "./repo/Packages.gz"
-bzip2 -9c "./repo/Packages" > "./repo/Packages.bz2"
+gzip -9c "./Packages" > "./Packages.gz"
+bzip2 -9c "./Packages" > "./Packages.bz2"
 
 # 清理临时目录
 rm -rf "$temp_packages"
